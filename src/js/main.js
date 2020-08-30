b5fefbeb0a06eb9441f2a84dec8c6839b8644be1
Hakan Bilgin <hbi99@hotmail.com>
2020-08-30 19:57:37 +0200

    media device connected to app

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,25 @@ const facetime = {
 	init() {
 		// fast references
 		this.content = window.find("content");
+		this.video = window.find("video");
 	},
 	dispatch(event) {
+		let Self = facetime,
+			el;
 		switch (event.type) {
 			case "window.open":
+				navigator.mediaDevices.getUserMedia({
+						video: true,
+						audio: true
+					}).then(stream => {
+						let video = this.video[0];
+						
+						video.srcObject = stream;
+						video.muted = true;
+						video.addEventListener("loadedmetadata", () => {
+							video.play();
+						});
+					});
 				break;
 		}
 	}
