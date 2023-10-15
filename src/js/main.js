b712094ec1c1419ec5770c37443f3141db19c02e
Hakan Bilgin <hbi99@hotmail.com>
2023-10-15 11:33:29 +0200

    app reset

--- src/js/main.js
+++ src/js/main.js
@@ -1,92 +1,11 @@
 
-@import "modules/sidebar.js";
-@import "modules/call.js";
-
-
-const defaultSettings = {
-	"clear-history-log": 604800,  // Seven days: 7*24*60*60
-};
-
-
-const ME = karaqu.user;
-
 const bell = {
-	els: {},
 	init() {
-		// fast references
-		this.els = {
-			content: window.find("content"),
-			sidebar: window.find(".sidebar"),
-			sidebarToggler: window.find(".sidebar-toggler"),
-			callList: window.find(".call-list .list-wrapper"),
-			videoCall: window.find(".video-call"),
-			videoMe: window.find(".me video"),
-			videoOther: window.find(".other"),
-		};
+		// init all sub-objects
+		Object.keys(this)
+			.filter(i => typeof this[i].init === "function")
+			.map(i => this[i].init());
 
-		// init auxilliary objects
-		Sidebar.init();
-		Call.init();
-
-		// auto mute video elements
-		// this.els.content.find("video").map(el => { el.muted = true; });
-
-		let Self = this;
-		// check storage for previously saved data
-		window.storage.getItem("call-history")
-			.then(storageData => {
-				// reference to history XML
-				Self.xHistory = window.bluePrint.selectSingleNode("//History");
-
-				if (storageData) {
-					// replace bluePrint data with storage data
-					Self.xHistory.parentNode.replaceChild(storageData, Self.xHistory);
-					Self.xHistory = window.bluePrint.selectSingleNode(`//History`);
-				}
-				// get settings, if any
-				Self.settings = window.settings.getItem("settings") || defaultSettings;
-				// auto clear call log
-				let arg = Self.settings["clear-history-log"];
-				Sidebar.dispatch({ type: "clear-history-log", arg });
-				// translate time stamps
-				Self.fixTimestamp();
-				// auto click "all" tab
-				window.find(".tab-row > div[data-arg='all']").trigger("click");
-				// fix menu options
-				window.bluePrint.selectNodes(`//Menu[@check-group="clear-log"]`).map(xMenu =>
-					(+xMenu.getAttribute("arg") === +arg)
-						? xMenu.setAttribute("is-checked", 1)
-						: xMenu.removeAttribute("is-checked"));
-			});
-
-		/* temp
-		this.dispatch({ type: "toggle-sidebar", value: "hide" });
-
-		setTimeout(() => {
-			Sidebar.dispatch({
-				type: "history-log-call",
-				data: {
-					user1: "hbi",
-					user2: "bill",
-					type: "voice",
-					stamp: 1595306176929,
-					duration: 0,
-				}
-			});
-			
-			setTimeout(() => {
-				bell.dispatch({ type: "toggle-sidebar", value: "show" });
-			}, 100);
-		}, 1500);
-		*/
-
-		// setTimeout(() => window.find(`div[data-arg="friends"]`).trigger("click"), 800);
-	},
-	fixTimestamp() {
-		this.xHistory.selectNodes("./i").map(call => {
-			let timestamp = new karaqu.Moment(+call.getAttribute("stamp"));
-			call.setAttribute("timestamp", timestamp.format("ddd D MMM, HH:mm"));
-		});
 	},
 	dispatch(event) {
 		let Self = bell,
@@ -94,52 +13,11 @@ const bell = {
 		switch (event.type) {
 			// system events
 			case "window.init":
-				// return;
-				// initiate camera
-				navigator.mediaDevices
-					.getUserMedia({ video: true, audio: true })
-					.then(stream => {
-						let video = Self.els.videoMe[0];
-						
-						Self.stream = stream;
-
-						video.srcObject = stream;
-						video.muted = karaqu.env === "dev";
-						video.addEventListener("loadedmetadata", () => {
-							video.play();
-						});
-					});
 				break;
-			case "window.close":
-				// disconnect camera stream
-				if (Self.stream) {
-					Self.els.videoMe[0].src = "";
-					Self.stream.getTracks().map(item => item.stop());
-				}
-				// save settings
-				window.settings.setItem("settings", Self.settings);
-				// save call log
-				window.storage.setItem("call-history", Self.xHistory);
-				break;
-			case "net.receive":
-				// forward event to Call-object
-				Call.receive(event);
-				break;
-			// custom events
-			case "clear-history-log":
-			case "toggle-sidebar":
-				Sidebar.dispatch(event);
-				break;
-			default:
-				el = event.el ? event.el.parents("[data-area]") : false;
-				// proxy event
-				if (el && el.hasClass("sidebar")) {
-					Sidebar.dispatch(event);
-				} else {
-					Call.dispatch(event);
-				}
 		}
-	}
+	},
+	call: @import "./modules/call.js",
+	sidebar: @import "./modules/sidebar.js",
 };
 
 window.exports = bell;
--- src/js/modules/sidebar.js
+++ src/js/modules/sidebar.js
@@ -1,158 +1,19 @@
 
-const Sidebar = {
+// bell.sidebar
+
+{
 	init() {
-		// listen to system event
-		karaqu.on("sys:friend-status", this.dispatch);
+		
 	},
 	dispatch(event) {
 		let APP = bell,
-			Self = Sidebar,
-			data,
-			xPath,
-			xNode,
-			target,
-			prepend,
-			user,
-			value,
-			isOn,
-			str,
-			pEl,
+			Self = APP.sidebar,
 			el;
+		// console.log(event);
 		switch (event.type) {
-			// system events
-			case "friend-status":
-				user = event.detail.username;
-				value = event.detail.status === 1 ? "online" : "offline";
-				el = APP.els.callList.find(`div[data-username="${user}"]`);
-				// update user status
-				el.removeClass("online offline").addClass(value);
-				break;
 			// custom events
-			case "clear-history-log":
-				value = +event.arg;
-
-				switch (value) {
-					case 0: // all
-						xPath = `./*`;
-						break;
-					case -1: // keep all
-						xPath = `./*[@duration = ${value}]`;
-						// save to settings
-						APP.settings["clear-history-log"] = event.arg;
-						break;
-					case -2: // clear missed only
-						xPath = `./*[@inbound = 1][@duration = 0]`;
-						break;
-					default:
-						// clear older than "seconds"
-						value = Date.now() - (value * 1000);
-						xPath = `./*[position() > 20][@stamp < "${value}"]`;
-						// save to settings
-						APP.settings["clear-history-log"] = event.arg;
-				}
-				// clear entries from DOM & xHistory
-				APP.xHistory.selectNodes(xPath).map(xEntry => {
-					let stamp = xEntry.getAttribute("stamp"),
-						el = APP.els.sidebar.find(`.call-entry[data-stamp="${stamp}"]`);
-					// remove from DOM
-					el.remove();
-					// remove from xHistory
-					xEntry.parentNode.removeChild(xEntry);
-				});
-				break;
-			case "start-camera-call":
-			case "start-voice-call":
-				Call.dispatch(event);
-				break;
-			case "history-log-call":
-				let meCalling = ME.username === event.data.user1,
-					username = meCalling ? event.data.user2 : event.data.user1,
-					inbound = ME.username === event.data.user2 ? 1 : 0;
-				str = `<i username="${username}"
-							inbound="${inbound}"
-							type="${event.data.type}"
-							stamp="${event.data.stamp}"
-							duration="${event.data.duration}"
-							_new="1"/>`;
-				// add entry to call log
-				xNode = APP.xHistory.insertBefore($.nodeFromString(str), APP.xHistory.firstChild);
-
-				// translate time stamps
-				APP.fixTimestamp();
-				// list wrapper
-				prepend = APP.els.callList;
-				// skip adding if friends tab is active
-				if (prepend.hasClass("list-friends")) return;
-				// add entry to DOM
-				el = window.render({
-							template: "call-entry",
-							match: `//Data/History/*[1]`,
-							prepend,
-						});
-				// remove attribute from log entry
-				xNode.removeAttribute("_new");
-				// remove from 
-				if (prepend.hasClass("list-friends") && (meCalling || event.data.duration > 0)) {
-					el.remove();
-				}
-				break;
-			case "toggle-sidebar":
-				if (event.value === "show") isOn = false;
-				isOn = isOn || APP.els.sidebarToggler.hasClass("push-in");
-				APP.els.sidebarToggler.toggleClass("push-in", isOn);
-
-				if (isOn) {
-					APP.els.sidebar.removeClass("open");
-				} else {
-					APP.els.sidebar.cssSequence("open", "transitionend", sEl => {
-						el = APP.els.callList.find(".anim-entry-prepend");
-						if (event.value !== "show" || !el.length) return;
-						// animate call entry
-						el.cssSequence("entry-reveal", "transitionend", el =>
-							el.removeClass("anim-entry-prepend entry-reveal"));
-					});
-				}
-				break;
-			case "select-tab":
-				el = event.el;
-				value = el.data("arg");
-				if (el.hasClass("tab-active")) return;
-
-				el.parent().find(".tab-active").removeClass("tab-active");
-				el.addClass("tab-active");
-
-				target = APP.els.callList.removeClass("list-all list-missed list-friends");
-				target.addClass(`list-${value}`);
-				
-				// render channels
-				switch (value) {
-					case "all":
-						window.render({
-							template: "calls",
-							match: "//Data/History",
-							changePath: "/xsl:for-each",
-							changeSelect: "./*",
-							target
-						});
-						break;
-					case "missed":
-						window.render({
-							template: "calls",
-							match: "//Data/History",
-							changePath: "/xsl:for-each",
-							changeSelect: "./*[@inbound = 1][@duration = 0]",
-							target
-						});
-						break;
-					case "friends":
-						window.render({
-							template: "friends",
-							match: "sys://Settings/Friends",
-							target
-						});
-						break;
-				}
+			case "some-event":
 				break;
 		}
 	}
-};
+}
